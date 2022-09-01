d923cd24a81eb31abb97b970ffff69b2f5d4d58c
Sergey Petrinich <petrinichshadow@gmail.com>
2022-09-01 20:30:23 +0300

    added redux-persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,26 @@
 import axios from "axios";
 import { createStore, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
 import { rootReducer } from "./rootReducer";
 import * as api from "../config";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const persistConfig = {
+    key: "root",
+    storage,
+    // обычно используется один лист
+    whitelist: ["theme"], // сохраняет определенные редюсеры
+    // blacklist:[] // удаляет определенные редюсеры
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = createStore(
-    rootReducer,
+    persistedReducer,
     composeEnhancers(
         applyMiddleware(
             thunk.withExtraArgument({
@@ -20,3 +32,4 @@ const store = createStore(
 );
 
 export { store };
+export const persistor = persistStore(store);
